7a55c0c88b54d10f79a352b0e93b78be7b6d34c9
Vladlan <lanvlad911@gmail.com>
2018-02-19 22:25:16 +0300

    second and last commit

--- ChangePasswordForm.js
+++ ChangePasswordForm.js
@@ -1,28 +1,59 @@
 import React, {Component} from "react";
 import "./ChangePassworedForm.css";
+import {connect} from 'react-redux';
 
+import * as actionCreators from './actions/actions'
 
 class ChangePasswordForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            incorrectCurrentPassword: null,
+            ifCurrentPasswordEqualNewPassword:null
+        };
+    };
+
     onSubmit(e) {
         e.preventDefault();
-        let currentPassword = this.refs.currentPassword.value;
+        let currentPasswordFromInput = this.refs.currentPassword.value;
+        let currentPassword = this.props.state.currentPassword;
         let newPassword = this.refs.newPassword.value;
-
-
+        if (currentPassword !== currentPasswordFromInput) {
+            let incorrectCurrentPassword2 = (<label style={{color:"red"}}>Incorrect current password</label>);
+            this.setState({incorrectCurrentPassword:incorrectCurrentPassword2});
+            this.refs.newPassword.value = this.refs.currentPassword.value = "";
+        }
+        else if (currentPassword === newPassword) {
+            let ifCurrentPasswordEqualNewPassword2 = (
+                <label style={{color:"red"}}>Current password is equal to new password</label>
+            );
+            this.setState({ifCurrentPasswordEqualNewPassword:ifCurrentPasswordEqualNewPassword2});
+            this.refs.newPassword.value = this.refs.currentPassword.value = "";
+        }
+        else {
+            this.props.changePassword(newPassword);
+            this.refs.newPassword.value = this.refs.currentPassword.value = "";
+            this.setState({ifCurrentPasswordEqualNewPassword:null});
+            this.setState({incorrectCurrentPassword:null});
+        }
     }
     
     render() {
         console.log(this.props);
+        let incorrectCurrentPassword = this.state.incorrectCurrentPassword;
+        let ifCurrentPasswordEqualNewPassword = this.state.ifCurrentPasswordEqualNewPassword;
+
         return (
             <div className="formContainer">
                 <span>My form Current password is {this.props.state.currentPassword}</span>
                 <form
-                    action="">
+                    onSubmit={this.onSubmit.bind(this)}>
                     <label htmlFor="">SET CURRENT PASSWORD</label>
                     <br/>
                     <input type="password"
                            placeholder="enter current password"
                            ref="currentPassword"/>
+
                     <br/>
 
                     <label htmlFor="">SET NEW PASSWORD</label>
@@ -32,17 +63,28 @@ class ChangePasswordForm extends Component {
                         type="password"
                         ref="newPassword"
                     />
+                    {ifCurrentPasswordEqualNewPassword}
+                    {incorrectCurrentPassword}
                     <br/>
 
-
                     <button>Change password</button>
-
 
                 </form>
             </div>
         )
     }
-
 }
 
-export default ChangePasswordForm;
+const mapStateToProps = state => {
+    return {
+        state: state,
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        changePassword: (newPassword) => dispatch(actionCreators.changePassword(newPassword))
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ChangePasswordForm);
--- index.js
+++ index.js
@@ -6,35 +6,18 @@ import ChangePasswordForm from './ChangePasswordForm';
 import reducer from './store/reducer.js';
 import {Provider} from 'react-redux'
 import {createStore} from 'redux';
-import {connect} from 'react-redux';
 
-import * as actionCreators from './actions/actions'
 
 const store = createStore(
     reducer, /* preloadedState, */
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
+
 const App = () => {
-    console.log(store);
-   return <ChangePasswordForm
-   state={store.getState()}/>
+   return <ChangePasswordForm/>
 };
-
-const mapStateToProps = state => {
-    console.log(state);
-    return {
-        state: state,
-    };
-};
-
-const mapDispatchToProps = dispatch => {
-    return {
-        changePassword: (newPassword) => dispatch(actionCreators.changePassword(newPassword))
-    };
-};
-
-connect(mapStateToProps, mapDispatchToProps)(App);
 
 render(
     (<Provider store={store}>
